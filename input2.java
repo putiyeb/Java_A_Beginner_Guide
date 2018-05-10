6ee775fef6c66fd8e628c801a1fcbc50ab3cb487
putiyeb <putiyeb@lillemap.net>
2018-05-10 23:41:49 +0800

    Create input2.java

---
+++ input2.java
@@ -0,0 +1,12 @@
+class Test {
+	// test jaca
+	public static void main(String args[]) throws java.io.IOException {
+			char ch;
+			System.out.print("The input character: ");
+			ch = (char) System.in.read();
+			if (ch == 'z') System.out.println("RIGHT");
+			else{
+				System.out.println("False");
+			}
+	}
+}
