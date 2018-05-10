0d2e2f2d751c6e2338ae88984819a12ebe3d4546
putiyeb <putiyeb@lillemap.net>
2018-05-10 23:12:05 +0800

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,14 @@
+class Test {
+	// test jaca
+	public static void main(String args[]) throws java.io.IOException {
+			char ch;
+			System.out.print("The input character: ");
+			ch = (char) System.in.read();
+			System.out.println("The character: "+ch);
+	}
+}
+
+output:
+
+The input character: fdgdfg
+The character: f
