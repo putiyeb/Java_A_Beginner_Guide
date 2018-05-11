e9f6fc1443e385723067b6e5ed79c613d5b1efe4
putiyeb <putiyeb@lillemap.net>
2018-05-12 07:34:06 +0800

    Create MultiLoop.java

---
+++ MultiLoop.java
@@ -0,0 +1,18 @@
+class Test {
+	// test jaca
+	public static void main(String args[]) {
+		double a,b,c;
+		for(a=1,b=10;b<=50;a++,b+=10){
+				System.out.println("The ten times of "+a+" is: "+b);
+		}
+	}
+}
+
+
+output:
+
+The ten times of 1.0 is: 10.0
+The ten times of 2.0 is: 20.0
+The ten times of 3.0 is: 30.0
+The ten times of 4.0 is: 40.0
+The ten times of 5.0 is: 50.0
