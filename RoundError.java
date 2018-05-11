f01989030b4d1ca5e17c76a731063dbc28c2bec7
putiyeb <putiyeb@lillemap.net>
2018-05-11 20:36:57 +0800

    Create RoundError.java

---
+++ RoundError.java
@@ -0,0 +1,120 @@
+class Test {
+	// test jaca
+	public static void main(String args[]) {
+		double a,b,c;
+		for(a=1;a<=100;a++){
+			b=Math.sqrt(a);
+			c=a-b*b;
+			if(c!=0){
+				System.out.println("The root of "+a+" is: "+b);
+				System.out.println("The rounding error is: "+c);
+			}
+		}
+	}
+}
+
+output:
+
+
+The root of 2.0 is: 1.4142135623730951
+The rounding error is: -4.440892098500626E-16
+The root of 3.0 is: 1.7320508075688772
+The rounding error is: 4.440892098500626E-16
+The root of 5.0 is: 2.23606797749979
+The rounding error is: -8.881784197001252E-16
+The root of 6.0 is: 2.449489742783178
+The rounding error is: 8.881784197001252E-16
+The root of 7.0 is: 2.6457513110645907
+The rounding error is: -8.881784197001252E-16
+The root of 8.0 is: 2.8284271247461903
+The rounding error is: -1.7763568394002505E-15
+The root of 10.0 is: 3.1622776601683795
+The rounding error is: -1.7763568394002505E-15
+The root of 12.0 is: 3.4641016151377544
+The rounding error is: 1.7763568394002505E-15
+The root of 13.0 is: 3.605551275463989
+The rounding error is: 1.7763568394002505E-15
+The root of 15.0 is: 3.872983346207417
+The rounding error is: -1.7763568394002505E-15
+The root of 18.0 is: 4.242640687119285
+The rounding error is: 3.552713678800501E-15
+The root of 19.0 is: 4.358898943540674
+The rounding error is: -3.552713678800501E-15
+The root of 20.0 is: 4.47213595499958
+The rounding error is: -3.552713678800501E-15
+The root of 23.0 is: 4.795831523312719
+The rounding error is: 3.552713678800501E-15
+The root of 24.0 is: 4.898979485566356
+The rounding error is: 3.552713678800501E-15
+The root of 26.0 is: 5.0990195135927845
+The rounding error is: 3.552713678800501E-15
+The root of 28.0 is: 5.291502622129181
+The rounding error is: -3.552713678800501E-15
+The root of 29.0 is: 5.385164807134504
+The rounding error is: 3.552713678800501E-15
+The root of 31.0 is: 5.5677643628300215
+The rounding error is: 3.552713678800501E-15
+The root of 32.0 is: 5.656854249492381
+The rounding error is: -7.105427357601002E-15
+The root of 37.0 is: 6.082762530298219
+The rounding error is: 7.105427357601002E-15
+The root of 38.0 is: 6.164414002968976
+The rounding error is: 7.105427357601002E-15
+The root of 40.0 is: 6.324555320336759
+The rounding error is: -7.105427357601002E-15
+The root of 43.0 is: 6.557438524302
+The rounding error is: 7.105427357601002E-15
+The root of 45.0 is: 6.708203932499369
+The rounding error is: -7.105427357601002E-15
+The root of 48.0 is: 6.928203230275509
+The rounding error is: 7.105427357601002E-15
+The root of 50.0 is: 7.0710678118654755
+The rounding error is: -7.105427357601002E-15
+The root of 51.0 is: 7.14142842854285
+The rounding error is: -7.105427357601002E-15
+The root of 52.0 is: 7.211102550927978
+The rounding error is: 7.105427357601002E-15
+The root of 58.0 is: 7.615773105863909
+The rounding error is: -7.105427357601002E-15
+The root of 59.0 is: 7.681145747868608
+The rounding error is: 7.105427357601002E-15
+The root of 60.0 is: 7.745966692414834
+The rounding error is: -7.105427357601002E-15
+The root of 61.0 is: 7.810249675906654
+The rounding error is: 7.105427357601002E-15
+The root of 63.0 is: 7.937253933193772
+The rounding error is: -7.105427357601002E-15
+The root of 65.0 is: 8.06225774829855
+The rounding error is: 1.4210854715202004E-14
+The root of 66.0 is: 8.12403840463596
+The rounding error is: -1.4210854715202004E-14
+The root of 72.0 is: 8.48528137423857
+The rounding error is: 1.4210854715202004E-14
+The root of 73.0 is: 8.54400374531753
+The rounding error is: 1.4210854715202004E-14
+The root of 75.0 is: 8.660254037844387
+The rounding error is: -1.4210854715202004E-14
+The root of 76.0 is: 8.717797887081348
+The rounding error is: -1.4210854715202004E-14
+The root of 77.0 is: 8.774964387392123
+The rounding error is: -1.4210854715202004E-14
+The root of 78.0 is: 8.831760866327848
+The rounding error is: -1.4210854715202004E-14
+The root of 80.0 is: 8.94427190999916
+The rounding error is: -1.4210854715202004E-14
+The root of 82.0 is: 9.055385138137417
+The rounding error is: -1.4210854715202004E-14
+The root of 87.0 is: 9.327379053088816
+The rounding error is: -1.4210854715202004E-14
+The root of 89.0 is: 9.433981132056603
+The rounding error is: 1.4210854715202004E-14
+The root of 92.0 is: 9.591663046625438
+The rounding error is: 1.4210854715202004E-14
+The root of 94.0 is: 9.695359714832659
+The rounding error is: -1.4210854715202004E-14
+The root of 95.0 is: 9.746794344808963
+The rounding error is: 1.4210854715202004E-14
+The root of 96.0 is: 9.797958971132712
+The rounding error is: 1.4210854715202004E-14
+The root of 97.0 is: 9.848857801796104
+The rounding error is: 1.4210854715202004E-14
