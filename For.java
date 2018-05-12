954e98335310a8de8020da354abe3e7af4f5ca29
putiyeb <putiyeb@lillemap.net>
2018-05-12 12:34:22 +0800

    Update For.java

--- For.java
+++ For.java
@@ -20,3 +20,38 @@ Right!
 $ 
 
 ?? why does the "False" print out twice 
+
+
+class Test {
+	// test jaca
+	public static void main(String args[]) throws java.io.IOException {
+		System.out.print("Please enter the character:");
+		char ch;
+		for(int a=1;(ch=(char)System.in.read())!='S';a++){
+			if(ch!='\n') continue;
+			System.out.println("False");
+		}
+		System.out.println("Right!");
+	}
+}
+
+output:
+
+$ java Test
+Please enter the character:ffghfg
+False
+fghfghh
+False
+t
+False
+rt
+False
+g
+False
+f
+False
+rthrthrth
+False
+Sregrthrth
+Right!
+$ 
