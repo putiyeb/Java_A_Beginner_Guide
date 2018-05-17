50a010e28949bc479ea419bb5b04d722cf6063cd
putiyeb <putiyeb@lillemap.net>
2018-05-17 20:43:36 +0800

    Update VehicleDemo.java

--- VehicleDemo.java
+++ VehicleDemo.java
@@ -47,3 +47,34 @@ class Test {
 output:
 
 8	10	is	80
+
+
+
+class Vehicle {
+
+	Vehicle(int a, int b, int c){
+		passengers = a;
+		fuelcap = b;
+		mpg = c;
+	}
+
+	int passengers;
+	int fuelcap;
+	int mpg;
+
+	int range(){
+		return fuelcap*mpg;
+	}
+}
+
+class Test {
+	public static void main(String args[]) {
+		Vehicle minivan = new Vehicle(4,8,10);
+		int a = minivan.range();
+		System.out.print(minivan.fuelcap+"\t"+minivan.mpg+"\tis\t"+a);
+	}
+}
+
+output:
+
+8	10	is	80
