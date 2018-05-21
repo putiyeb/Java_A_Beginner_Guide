2f9c739b425c5dcd0cb6b964dd37f8a8d0de1eb4
putiyeb <putiyeb@lillemap.net>
2018-05-21 20:39:51 +0800

    Create TwoDArray.java

---
+++ TwoDArray.java
@@ -0,0 +1,27 @@
+class Test {
+	public static void main(String args[]) {
+		int a[][] = new int[3][4];
+		for(int i=0;i<3;i++){
+			for(int j=0;j<4;j++){
+				System.out.println(a[i][j]=4*i+j+1);
+			}
+		}
+	}
+}
+
+
+output:
+
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
