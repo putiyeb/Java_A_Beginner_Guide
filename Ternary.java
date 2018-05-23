130ea1f4928cd279f9f2750be1d823e7946659d0
putiyeb <putiyeb@lillemap.net>
2018-05-24 00:24:34 +0800

    Create Ternary.java

---
+++ Ternary.java
@@ -0,0 +1,19 @@
+class Test {
+	public static void main(String args[]) {
+		for(int i=0;i<10;i++){
+			if(i!=0?true:false) System.out.println(100/i);
+		}
+	}
+}
+
+output:
+
+100
+50
+33
+25
+20
+16
+14
+12
+11
