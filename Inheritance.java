75661697a9e5077ffd61c136207cbbba86a89836
putiyeb <putiyeb@lillemap.net>
2018-06-06 20:08:27 +0800

    Create Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,15 @@
+class sub {
+	public static int a = 7;
+	public static int b = 6;
+}
+
+class Test extends sub {
+	public static void main(String args[]){
+		System.out.println(a+"\t"+b);
+	}
+}
+
+
+output:
+
+7	6
