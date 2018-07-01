2bc27aeb8c792f2b0bafaa355cc23d3fd8a6be52
putiyeb <putiyeb@lillemap.net>
2018-07-01 18:12:48 +0800

    Update NestExceptionThrow.java

--- NestExceptionThrow.java
+++ NestExceptionThrow.java
@@ -40,3 +40,56 @@ class Test{
 $ java Test
 BAD
 */
+
+
+class Q extends Exception{
+    public String toString(){
+        return "GOOD";
+    }
+}
+
+class P extends Q{
+    public String toString(){
+        return "BAD";
+    }
+}
+
+class Test{
+    public static void main(String args[]){
+        try{
+            for(int i = 0; i<3; i++){
+                try{
+                    if(i == 1){
+                        throw new Q();
+                    }else{
+                        throw new P();
+                    }
+                }
+                catch(Q exc){
+                    throw exc;
+                }
+                finally{
+                    System.out.println("3");
+                }
+            }
+        }
+        catch(Q exc){
+            System.out.println(exc);
+        }
+        catch(Throwable exc){
+            System.out.println("1");
+        }
+        finally{
+            System.out.println("3");
+        }
+    }
+}
+
+
+/*
+$ java Test
+3
+BAD
+3
+
+*/
