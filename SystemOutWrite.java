e57439993876cb5331fc1f177e5e6f1ff4edf613
putiyeb <putiyeb@lillemap.net>
2018-07-02 22:18:56 +0800

    Create SystemOutWrite.java

---
+++ SystemOutWrite.java
@@ -0,0 +1,27 @@
+class Test{
+    public static void main(String args[]){
+        int b;
+        b = 'F';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
+
+/*
+$ java Test
+F
+*/
+
+
+class Test{
+    public static void main(String args[]){
+        int b;
+        b = 'F';
+        System.out.write(b);
+    }
+}
+
+/*
+$ java Test
+$
+*/
