c4b1e33c98a5a38083718823d61cadcee0ac0c53
putiyeb <putiyeb@lillemap.net>
2018-07-04 23:55:58 +0800

    Update TryWithResource.java

--- TryWithResource.java
+++ TryWithResource.java
@@ -26,3 +26,35 @@ class Test{
 $ java Test 1.txt
 sdfrg rg erger df234245
 */
+
+
+import java.io.*;
+
+class Test{
+    public static void main(String args[]){
+        int i;
+
+        if(args.length != 2){
+            System.out.println("At least two file");
+            return;
+        }
+
+        try(FileInputStream fin = new FileInputStream(args[0]);
+        FileOutputStream fout = new FileOutputStream(args[1])){
+            do{
+                i = fin.read();
+                if(i != -1) fout.write(i);
+            }while(i != -1);
+        }
+        catch(IOException exc){
+            System.out.println("The IO error is:" + exc);
+        }
+    }
+}
+
+
+/*
+$ java Test 1.txt 2.txt
+$ cat 2.txt
+sdfrg rg erger df234245
+*/
