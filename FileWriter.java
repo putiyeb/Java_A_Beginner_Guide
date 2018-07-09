16caac37f42c89797429c77a4b95b46ff560197c
putiyeb <putiyeb@lillemap.net>
2018-07-09 19:44:43 +0800

    Update FileWriter.java

--- FileWriter.java
+++ FileWriter.java
@@ -44,3 +44,57 @@ $ cat testdata3
 $
 
 */
+
+
+import java.io.*;
+
+class Test{
+    public static void main(String args[]) throws IOException{
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        FileWriter fw = new FileWriter("testdata3",true);
+
+        String str;
+
+        try{
+
+        do{
+            str = bf.readLine();
+            pw.println("\n\t" + str);
+            if(str.equals("stop")) break;
+            fw.write(str + "\r\n");
+            fw.flush();
+        }while(str.compareTo("stop") != 0);
+
+        fw.close();
+
+        }
+        catch(IOException exc){
+            pw.println(exc);
+        }
+
+    }
+}
+
+/*
+$ java Test
+sdferg
+
+	sdferg
+egerge
+
+	egerge
+stop
+
+	stop
+$
+
+$ cat testdata3
+sdfergrgsdferg
+egerge
+$
+
+*/
