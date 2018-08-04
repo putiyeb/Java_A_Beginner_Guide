239ce84ca358592f96b0384ac1403c5efd7bc03c
putiyeb <putiyeb@lillemap.net>
2018-08-04 22:16:09 +0800

    Create CompileAndRun.java

---
+++ mymodapp2/CompileAndRun.java
@@ -0,0 +1,5 @@
+$ javac --module-source-path appsrc -d appmodules appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java 
+$ java --module-path appmodules -m appstart/appstart.mymodappdemo.MyModAppDemo
+2 is a factor of 10
+1
+1
