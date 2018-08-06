8baa2fc04703626de44ef82673769e615c288f09
putiyeb <putiyeb@lillemap.net>
2018-08-06 11:23:27 +0800

    Create ComplieAndRun.java

---
+++ mymodapp3/ComplieAndRun.java
@@ -0,0 +1,7 @@
+$ javac -d appmodules --module-source-path appsrc appsrc/userfuncsimp/module-info.java appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java 
+$ java --module-path appmodules -m appstart/appstart.mymodappdemo.MyModAppDemo
+2 is a factor of 10
+1
+1
+Result of absPlus function: 16
+Result of absMinus function: 8
